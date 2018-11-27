6a925ad4d4ffb2d7192da030389696cd63134e40
jackwong <wangchangat@163.com>
2018-11-27 19:32:45 +0800

    init

---
+++ src/Tools/JWTools.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jack
+ * Date: 2018/11/27
+ * Time: 19:25
+ */
+
+namespace Jack\Tools;
+
+class JWTools{
+    public function test()
+    {
+        echo 'This is Demo';
+    }
+}
